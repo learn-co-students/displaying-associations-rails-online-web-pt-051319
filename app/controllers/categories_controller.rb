9bebe75da94c1bb89aedba10349ed68b0cb6dc93
Jasmine <jasmine1226@gmail.com>
2019-10-10 19:37:03 -0700

    Done.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -21,8 +21,13 @@ class CategoriesController < ApplicationController
   end
 
   def update
-    category = Category.find(params[:id])
-    category.update(params.require(:category))
-    redirect_to category_path(category)
+    @category = Category.find(params[:id])
+    @category.update(params.require(:category).permit(:name))
+
+    if @category.valid?
+      redirect_to category_path(@category)
+    else
+      render :edit
+    end
   end
 end
